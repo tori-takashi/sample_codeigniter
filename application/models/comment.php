1d56e05483e1a7bc5c46a1b213e8de01ee5e8576
tori-takashi <tori.toutoshi@gmail.com>
2021-05-15 21:25:31 +0900

    create comment model

---
+++ application/models/comment.php
@@ -0,0 +1,10 @@
+<?php
+  class Comment_model extends CI_Model {
+      public function __construct() {
+          $this->load->database();
+      }
+
+      public function get_all_comments() {
+          return $this->db->get("comments");
+      }
+  }
