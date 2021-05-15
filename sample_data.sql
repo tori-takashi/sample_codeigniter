8b84f259087560f7944b3c25c72535ea6f0d2b63
tori-takashi <tori.toutoshi@gmail.com>
2021-05-15 21:23:32 +0900

    add schema and sample data

---
+++ sample_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO comments (user_name, comment) VALUES ("user1", "aaaaaaaaa");
+INSERT INTO comments (user_name, comment) VALUES ("user1", "bbbbbbbbb");
+INSERT INTO comments (user_name, comment) VALUES ("user2", "aaaaaaaaa");
+INSERT INTO comments (user_name, comment) VALUES ("user3", "aaaaaaaaa");
+INSERT INTO comments (user_name, comment) VALUES ("user4", "aaaaaaaaa");
+INSERT INTO comments (user_name, comment) VALUES ("user5", "ddddddddd");
--- schema.sql
+++ schema.sql
@@ -1,1 +1,7 @@
 USE sample_codeigniter;
+
+CREATE TABLE comments (
+    id int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_name varchar(20) NOT NULL,
+    comment VARCHAR(20) NOT NULL
+);
