0b136a36c3524bc3ca45bb5ec2b0768d5bf5e4d7
tori-takashi <tori.toutoshi@gmail.com>
2021-05-15 19:32:32 +0900

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get -y install wget
+sudo apt-get -y install php7.4-cli
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.14-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.14-1_all.deb
+sudo apt-get update
+sudo apt-get install mysql-community-server
+
+
+
+
