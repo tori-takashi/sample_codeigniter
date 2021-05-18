b173c6e5145029f5467499dc942b897fefc77e1c
tori-takashi <tori.toutoshi@gmail.com>
2021-05-18 16:01:44 +0900

    add post_date

--- application/models/Comment.php
+++ application/models/Comment.php
@@ -2,6 +2,7 @@
   class Comment extends CI_Model {
       public function __construct() {
           $this->load->database();
+          date_default_timezone_set('Asia/Tokyo');
       }
 
       public function get_all_comments() {
@@ -10,9 +11,13 @@
 
       public function set_comment() {
           $this->load->helper('url');
+
+          $post_date = date("Y-m-d H:i:s");
+
           $data = array(
               'user_name' => html_escape($this->input->post('view_name')),
-              'comment' => html_escape($this->input->post('message'))
+              'comment' => html_escape($this->input->post('message')),
+              'post_date' => $post_date
           );
 
           return $this->db->insert('comments', $data);
--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ USE sample_codeigniter;
 CREATE TABLE comments (
     id int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_name varchar(20) NOT NULL,
-    comment VARCHAR(20) NOT NULL
+    comment VARCHAR(20) NOT NULL,
+    post_date VARCHAR(40) NOT NULL
 );
